62ff2a950621e21a28dd6a18e06747d9f0992978
Brian Porter <beporter@users.sourceforge.net>
2011-03-05 20:16:42 -0600

    Basic Page CRUD almost complete.
    Completed the $_schema definitions for all "tables"
    Backpack supports.
    
    Refactored the HttpSocket requests out of ::read() 
    into a handler routine with override-able options 
    to handle the common generic stuff.
    
    Completed helpers for listing, reading, searching
    and creating Backpack Pages.
    
    Using ::read() to test the helpers.
    
    Signed-off-by: Brian Porter <beporter@users.sourceforge.net>

--- models/datasources/backpack_source.php
+++ models/datasources/backpack_source.php
@@ -65,6 +65,7 @@ class BackpackSource extends DataSource {
  * @access public
  */
 	protected $_schema = array(
+		// Pages -------------------------------
 		'pages' => array(
 			'id' => array(
 				'type' => 'integer',
@@ -75,38 +76,276 @@ class BackpackSource extends DataSource {
 			'title' => array(
 				'type' => 'string',
 				'null' => false,
-				'key' => '',
-				'length' => 255
+				'length' => 255,
 			),
 			'scope' => array(
 				'type' => 'string',
 				'null' => false,
-				'key' => '',
-				'length' => 255
+				'length' => 255,
+			),
+			'email_address' => array(
+				'type' => 'string',
+				'null' => false,
+				'length' => 255,
+				'readonly' => true,  // Special key to identify columns we can't change through the API.
 			),
 		),
-		//###TODO: Define the rest of the schemata.
+
+		// Lists -------------------------------
 		'lists' => array(
+			'id' => array(
+				'type' => 'integer',
+				'null' => false,
+				'key' => 'primary',
+				'length' => 11,
+			),
+			'name' => array(
+				'type' => 'string',
+				'null' => false,
+				'length' => 255,
+			),
 		),
+
+		// ListItems ---------------------------
 		'list_items' => array(
+			'id' => array(
+				'type' => 'integer',
+				'null' => false,
+				'key' => 'primary',
+				'length' => 11,
+			),
+			'completed' => array(
+				'type' => 'int',
+				'null' => false,
+				'length' => 1,
+				'default' => 0,
+			),
+			'body' => array(
+				'type' => 'string',
+				'null' => false,
+				'length' => 4096,
+			),
 		),
+
+		// Notes -------------------------------
 		'notes' => array(
+			'id' => array(
+				'type' => 'integer',
+				'null' => false,
+				'key' => 'primary',
+				'length' => 11,
+			),
+			'title' => array(
+				'type' => 'string',
+				'null' => false,
+				'length' => 255,
+				'default' => '',
+			),
+			'created_at' => array(
+				'type' => 'datetime',
+				'null' => false,
+			),
+			'body' => array(
+				'type' => 'string',
+				'null' => false,
+				'length' => 4096,
+				'default' => '',
+			),
 		),
+
+		// Separators --------------------------
 		'separators' => array(
+			'id' => array(
+				'type' => 'integer',
+				'null' => false,
+				'key' => 'primary',
+				'length' => 11,
+			),
+			'name' => array(
+				'type' => 'string',
+				'null' => false,
+				'length' => 255,
+				'default' => '',
+			),
+			'id' => array(
+				'type' => 'integer',
+				'null' => false,
+				'length' => 11,
+			),
 		),
+
+		// Tags --------------------------------
 		'tags' => array(
+			'id' => array(
+				'type' => 'integer',
+				'null' => false,
+				'key' => 'primary',
+				'length' => 11,
+			),
+			'name' => array(
+				'type' => 'string',
+				'null' => false,
+				'length' => 255,
+				'default' => '',
+			),
 		),
+
+		// Reminders ---------------------------
 		'reminders' => array(
+			'id' => array(
+				'type' => 'integer',
+				'null' => false,
+				'key' => 'primary',
+				'length' => 11,
+			),
+			'remind_at' => array(
+				'type' => 'datetime',
+				'null' => false,
+			),
+			'content' => array(
+				'type' => 'string',
+				'null' => false,
+				'length' => 255,
+				'default' => '',
+			),
+			'creator_id' => array(
+				'type' => 'integer',
+				'null' => false,
+				'length' => 11,
+			),
 		),
+
+		// Emails ------------------------------
 		'emails' => array(
+			'id' => array(
+				'type' => 'integer',
+				'null' => false,
+				'key' => 'primary',
+				'length' => 11,
+			),
+			'title' => array(
+				'type' => 'string',
+				'null' => false,
+				'length' => 255,
+				'default' => '',
+			),
+			'created_at' => array(
+				'type' => 'datetime',
+				'null' => false,
+			),
+			'body' => array(
+				'type' => 'string',
+				'null' => false,
+				'length' => 4096,
+				'default' => '',
+			),
 		),
+
+		// Statuses ----------------------------
 		'statuses' => array(
+			'id' => array(
+				'type' => 'integer',
+				'null' => false,
+				'key' => 'primary',
+				'length' => 11,
+			),
+			'message' => array(
+				'type' => 'string',
+				'null' => false,
+				'length' => 255,
+				'default' => '',
+			),
+			'created_at' => array(
+				'type' => 'datetime',
+				'null' => false,
+			),
+			'user_id' => array(
+				'type' => 'integer',
+				'null' => false,
+				'length' => 11,
+			),
 		),
+
+		// JournalEntries ----------------------
 		'journal_entries' => array(
+			'id' => array(
+				'type' => 'integer',
+				'null' => false,
+				'key' => 'primary',
+				'length' => 11,
+			),
+			'body' => array(
+				'type' => 'string',
+				'null' => false,
+				'length' => 4096,
+				'default' => '',
+			),
+			'created_at' => array(
+				'type' => 'datetime',
+				'null' => false,
+			),
+			'updated_at' => array(
+				'type' => 'datetime',
+				'null' => false,
+			),
+			'user_id' => array(
+				'type' => 'integer',
+				'null' => false,
+				'length' => 11,
+			),
 		),
+
+		// Users -------------------------------
 		'users' => array(
+			'id' => array(
+				'type' => 'integer',
+				'null' => false,
+				'key' => 'primary',
+				'length' => 11,
+			),
+			'name' => array(
+				'type' => 'string',
+				'null' => false,
+				'length' => 255,
+				'default' => '',
+			),
+			'start_page_id' => array(
+				'type' => 'integer',
+				'null' => false,
+				'length' => 11,
+			),
 		),
+
+		// Bookmarks ---------------------------
 		'bookmarks' => array(
+			'id' => array(
+				'type' => 'integer',
+				'null' => false,
+				'key' => 'primary',
+				'length' => 11,
+			),
+			'user_id' => array( 
+				'type' => 'string',
+				'null' => false,
+				'length' => 255,
+				'default' => '',
+			),
+			'bookmarkable_type' => array(
+				'type' => 'string',
+				'null' => false,
+				'length' => 50,
+				'default' => 'Page',
+			),
+			'bookmarkable_id' => array(
+				'type' => 'integer',
+				'null' => false,
+				'length' => 11,
+			),
+			'position' => array(
+				'type' => 'integer',
+				'null' => false,
+				'length' => 11,
+			),
 		),
 	);
 
@@ -117,6 +356,14 @@ class BackpackSource extends DataSource {
  * @access public
  */
 	public $connected = false;
+
+/**
+ * Last HTTP response code
+ *
+ * @var integer
+ * @access protected
+ */
+	protected $httpResponseCode = null;
 
 /**
  * Default configuration
@@ -132,12 +379,27 @@ class BackpackSource extends DataSource {
 	);
 
 /**
+ * Standard XML formatting options for all requests/responses.
+ *
+ * @var array
+ * @access private
+ */
+	private $xml_options = array(
+		'format' => 'tags', 
+		'slug' => false
+	);
+
+/**
  * HttpSocket instance
  *
  * @var HttpSocket
  * @access public
  */
-	public $connection = null;
+	public $Http = null;
+
+
+
+
 
 
 /**
@@ -160,11 +422,11 @@ class BackpackSource extends DataSource {
  * @access public
  */ 
 	public function connect() {
-		$this->connection = new HttpSocket(array(
+		$this->Http = new HttpSocket(array(
 								'timeout' => $this->config['timeout'],
 							));
 
-		if ($this->connection) {
+		if ($this->Http) {
 			$this->connected = true;
 		}
 		return $this->connected;
@@ -177,7 +439,7 @@ class BackpackSource extends DataSource {
  * @access public
  */
 	public function close() {
-		$this->connection = null;
+		$this->Http = null;
 		$this->connected = false;
 		return true;
 	}
@@ -188,7 +450,7 @@ class BackpackSource extends DataSource {
  * @return array Array containing table column information.
  * @access public
  */
-public function describe($model) {
+public function describe(&$model) {
 	if(isset($this->_schema[$model->alias])) {
 		return $this->_schema[$model->alias];
 	}
@@ -205,7 +467,144 @@ public function describe($model) {
 	public function listSources() {
 		return array_keys($this->_schema);
 	}
+
+
+
+
+
+
+
+/**
+ * Fetch a list of page records.
+ *
+ * @return mixed Array of Page records on success, false on HTTP request failure or 37s API failure.
+ * @access protected
+ */
+	protected function pages_list_all() {
+		
+		$request_overrides = array(
+				'uri' => array(
+					'path' => "/ws/pages/all",
+				),
+			);
 	
+		// Fire the http request.
+		if(($result = $this->_request($request_overrides)) === false) {
+			return false;
+		}		
+		
+		// Build a Cake-style array.
+		$results = array();
+		foreach($result['Response']['Pages']['Page'] as $index => $r)
+		{
+			$results[] = array($this->model->alias => $r);
+		}
+		
+		return $results;
+	}
+	
+/**
+ * Search for pages containing a specific term.
+ *
+ * @param string $term Search string. HTML characters will be sanitized.
+ * @return mixed Array of Page records on success, false on HTTP request failure or 37s API failure.
+ * @access protected
+ */
+	protected function pages_search($term) {
+		
+		$request_overrides = array(
+				'uri' => array(
+					'path' => "/ws/pages/search",
+				),
+				'body' => (string)$this->requestBody(array('term' => htmlentities($term))),
+			);
+	
+		// Fire the http request.
+		if(($result = $this->_request($request_overrides)) === false) {
+			return false;
+		}		
+		
+		// Build a Cake-style array.
+		$results = array();
+		foreach($result['Response']['Pages']['Page'] as $index => $r)
+		{
+			$results[] = array($this->model->alias => $r);
+		}
+		
+		return $results;
+	}
+	
+/**
+ * Create a new page.
+ *
+ * @param string $title Title of the new page.
+ * @param string $description Initial description of the page. (No longer supported?)
+ * @return mixed Array containing the new page, including ID if the page was created, false on failure.
+ * @access protected
+ */
+	protected function pages_create($title, $description = null) {
+		
+		$request_overrides = array(
+				'uri' => array(
+					'path' => "/ws/pages/new",
+				),
+				'body' => (string)$this->requestBody(array(
+									'page' => array(
+										'title' => htmlentities($title),
+										//'description' => htmlentities($description),
+									),
+								)),
+			);
+	
+		// Fire the http request.
+		if(($result = $this->_request($request_overrides)) === false) {
+			return false;
+		}		
+		
+		return array($this->model->alias => $result['Response']['Page']);
+	}
+	
+
+/**
+ * Fetch a single page with all associated data.
+ *
+ * @param int $page_id The ID number of the page to fetch.
+ * @return mixed Array containing the new page, false on failure.
+ * @access protected
+ */
+	protected function pages_show($page_id) {
+		
+		$request_overrides = array(
+				'uri' => array(
+					'path' => "/ws/page/".preg_replace('/[^0-9]/', '', $page_id),
+				),
+			);
+	
+		// Fire the http request.
+		if(($result = $this->_request($request_overrides)) === false) {
+			return false;
+		}		
+		
+		return array($this->model->alias => $result['Response']['Page']);
+	}
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
  * Create a new Backpack record based on the table name of $model
  *
@@ -221,9 +620,8 @@ public function describe($model) {
 		return false;
 	}
 
-
 /**
- * Fetch records from the Backpack table associated with the provided $model table.
+ * Read wrapper. Outsources actual lookups to API-call-specific helpers.
  *
  * @param Model $model The model being read.
  * @param array $queryData An array of query data used to find the data you want
@@ -231,68 +629,26 @@ public function describe($model) {
  * @access public
  */
 	function read(&$model, $queryData = array()) {
+		
 		if(!$this->connected) {
-			$this->log('not connected');
-			return 'happy';
-		}
-		
-		// Simple version
-		
-		//###DEBUG  ###TODO: Process the options from $queryData to determine what type of fetch we're doing. (all items, search, one item, etc.)
-		$action = 'all';
-		$table = $model->table;  //###TODO: Verify against $_schema.
-		
-		//###TODO: Abstract all this request setup stuff out for all CRUD methods to share. Everything except $body and $request[uri][path] are always the same.
-		$options = array(
-				'format' => 'tags', 
-				'slug' => false, 
-			);
-		
-		$body = array(
-				'request' => array(
-					'token' => "{$this->config['token']}",
-					//'term' => '',  //###TODO: Used for searching. Populate from [conditions].
-					),
-				);
-		$body = new Xml($body, $options);
-		
-		$request = array(
-					'method' => 'POST',
-					'uri' => array(
-						'scheme' => 'https', //($this->config['ssl'] == true ? 'https' : 'http'),
-						'host' => $this->config['host'],
-						'path' => "/ws/{$table}/{$action}",
-					),
-					'header' => array(
-						'Content-Type' => 'application/xml',
-					),
-					'body' => $body->toString(array('cdata' => false)),
-				);
-	
-		// Fire the http request.
-		if(($result = $this->connection->request($request)) === false) {
-			return false;
-		}		
-		
-		$result = new Xml($result, $options);
-		$result = $result->toArray($options);
-
-		// 37s server returned an error.
-		if(strpos($result['Response']['success'], 'true') === false) {
+			$this->log('not connected');  //###TODO
 			return false;
 		}
+		
+		$this->model = $model;
 
-		// Build a Cake-style array.
-		$result = $result['Response']['Pages']['Page'];  //###TODO: Ugh! Hardcoded for pages! Use $model->(table|name|alias) instead!
+
+		//###TODO: Parse the $queryData to figure out which helper to call!
 		
-		$results = array();
-		foreach($result as $index => $r)
-		{
-			$results[] = array($model->alias => $r);
-		}
+		//###DEBUG: Hardcoded for testing individual helpers!
 		
-		//###TODO: Honor "recursive" setting and build out associations. 37s does most of the hard work and usually returns everything together.
-		
+		// Pages:
+		//$results = $this->pages_list_all();
+		//$results = $this->pages_search('Mac');
+		//$results = $this->pages_create('Test3', 'Description is no longer supported. Use Notes instead.');
+		$results = $this->pages_show('836576');
+
+
 		return $results;
 	}
 
@@ -325,4 +681,73 @@ public function describe($model) {
 			$id = $model->id;
 		}
 	}
+
+/**
+ * XML request body generator. Creates a fully-formed XML string to submit to the API.
+ *
+ * @param array $additional_body_tags Associative array of additional body tags to include along with <token>. Example: array('term' => $searchterm).
+ * @return string An XML formatted string containing the <request>...<request> to send to the API.
+ * @access public
+ */
+	function requestBody($additional_body_tags = array()) {
+		$body = array(
+				'request' => array(
+					'token' => "{$this->config['token']}"
+				), 
+			);
+		$additional_body_tags = array('request' => $additional_body_tags);
+		$body = array_replace_recursive($body, $additional_body_tags);  //###VER: 5.3+ only!
+		$body = new Xml($body, $this->xml_options);
+		$body = $body->toString(array('cdata' => false, 'whitespace' => false));
+		return $body;
+	}
+
+/**
+ * Generic request processor.
+ *
+ * @param array $request_overrides Associated array matching HttpSocket $request options. Only include elements that need to be overridden for the given request.
+ * @return mixed Array of raw converted XML records on success, false on HTTP request failure or 37s API failure.
+ * @access public
+ */
+	protected function _request($request_overrides = null) {	
+		$request_defaults = array(
+				'method' => 'POST',
+				'uri' => array(
+					'scheme' => 'https', //($this->config['ssl'] == true ? 'https' : 'http'),
+					'host' => $this->config['host'],
+				),
+				'header' => array(
+					'Content-Type' => 'application/xml',
+				),
+				'body' => (string)$this->requestBody(),
+			);
+		
+		$request = array_replace_recursive($request_defaults, $request_overrides);  //###VER: 5.3+ only!
+		
+		//###DEBUG:	debug($request); exit;
+		
+		// Fire the http request.
+		if(($response = $this->Http->request($request)) === false) {
+			return false;
+		}	
+		
+		//###DEBUG:	debug($this->Http->response['raw']);
+	
+		// Set the HTTP response code for later use. (Can't determine success/failure from the code without knowing the specific request. DELETEs return a '204 No content' on success for example.)
+		$this->httpResponseCode = $this->Http->response['status']['code'];
+		
+		if( in_array($this->httpResponseCode, array(404,500,503)) ) {
+			return false;
+		}
+		// Convert the response to an array
+		$response = new Xml($response, $this->xml_options);
+		$response = $response->toArray($this->xml_options);
+		
+		// 37s server returned an error in the message body.
+		if(!isset($response['Response']['success']) || strpos($response['Response']['success'], 'false') !== false) {
+			return false;
+		}
+		return $response;
+	}
+
 }
